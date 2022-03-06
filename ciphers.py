5cc1c8ac76c58b4caa2c5e6ba8213c6ea0958a4e
queenmypawn <63803297+queenmypawn@users.noreply.github.com>
2022-03-06 11:31:43 -0500

    Create ciphers.py
    contains caesar and vignere ciphers.

---
+++ ciphers.py
@@ -0,0 +1,96 @@
+def lowercase_letter_rotation(plain_letter, rotational_shift):
+    current_letter_ord = ord(plain_letter)
+    new_letter_ord = current_letter_ord + rotational_shift - 97 # the new character's unicode, zero-indexed.
+    substitute = alphabet[new_letter_ord % 26] # the new letter in the English alphabet   
+    return(substitute) 
+
+def uppercase_letter_rotation(plain_letter, rotational_shift):
+    current_letter_ord = ord(plain_letter)
+    new_letter_ord = current_letter_ord + rotational_shift - 65 # the new character's unicode, zero-indexed.
+    substitute = alphabet_caps[new_letter_ord % 26] # the new letter in the English alphabet   
+    return(substitute) 
+
+def caesar_cipher(plaintext, key, all_lowercase = False, all_uppercase = False):
+
+# Verification needed that the key is an int and not a string.
+    #(Coming soon)
+
+    ciphered_text = []
+    size_of_plaintext = len(plaintext)
+
+    for i in range(size_of_plaintext):
+
+        current_letter = plaintext[i]
+
+        # Checks to see what type of character it is.
+        current_letter_ord = ord(current_letter)
+
+        # If it's a space, the cipher leaves it alone.
+        # If it's anything other than a letter, space or period, it throws an error.
+
+        # uppercase letters
+        if current_letter_ord >= 65 and current_letter_ord <= 90:
+            substitute = uppercase_letter_rotation(current_letter, key)
+            ciphered_text.append(substitute)
+
+        # lowercase letters
+        elif current_letter_ord >= 97 and current_letter_ord <= 122:
+            substitute = lowercase_letter_rotation(current_letter, key)
+            ciphered_text.append(substitute)
+
+        # space or period, respectively
+        elif current_letter_ord == 32 or current_letter_ord == 46:
+            ciphered_text.append(current_letter) # no changes necessary
+
+        # not a valid character.
+        else:
+            # throw an error
+            continue
+
+# if all_lowercase is true, return plaintext in all lowercase
+
+# if all_uppercase is true, return plaintext in all uppercase
+
+    return ''.join(ciphered_text) # converts the list to a string
+
+# The Vignere Cipher is a polyalphabetic extension of the Caesar Cipher.
+def vignere_cipher(plaintext, key, all_lowercase = False, all_uppercase = False):
+
+# Verify that the key is actually a string and not an int.
+    #(Coming soon)
+
+    ciphered_text = []
+
+    # Convert the string into a list of letters and perform the correct
+    # substitution on each depending on its position.
+    poly_sub_in_motion = True
+    counter = 0
+    size_of_plaintext = len(plaintext)
+
+    while (poly_sub_in_motion):
+
+        # Perform a check to see if we're at the end of the string (or if the string is empty).
+        if(counter == size_of_plaintext):
+            break
+
+        current_letter = plaintext[counter]
+
+        # Convert the current letter in the Vignere key to an ordinal
+        # to cipher the current plaintext letter.
+        current_vignere_letter = key[counter % len(key)]
+        current_vignere_ord = ord(current_vignere_letter)
+
+        # Perform the Caesar Cipher on the letter.
+        ciphered_text.append(caesar_cipher(current_letter, current_vignere_ord))
+
+        counter += 1
+    
+    return ''.join(ciphered_text) # converts the list to a string
+
+alphabet_caps = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' # 65-90
+alphabet = 'abcdefghijklmnopqrstuvwxyz' # 97-122
+
+phrase = 'test'
+
+print("Output of Caesar Cipher with a left shift of 350 is " + caesar_cipher(phrase, -7))
+print("Output of Vignere Cipher with a key shift of 'Misc' is " + vignere_cipher(phrase, 'abc'))
